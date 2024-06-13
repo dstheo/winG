f517b05ef8535ceea4e2a2625344c018b46ef2a2
Theo <theo963000@gmail.com>
2024-06-13 09:52:34 +0900

    Update descriptive_statistics_wgk.py

--- descriptive_statistics_wgk.py
+++ descriptive_statistics_wgk.py
@@ -4,7 +4,6 @@
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
-from matplotlib.ticker import MaxNLocator
 
 # 경로와 파일명
 data_path = 'data'
@@ -25,13 +24,6 @@ descriptive_stats = data.describe()
 descriptive_stats.loc['median'] = data.median()
 descriptive_stats.loc['mode'] = data.mode().iloc[0]
 
-# 히스토그램 서브플롯 크기 설정
-numeric_columns = data.select_dtypes(include = 'number').columns
-num_columns = len(numeric_columns)
-num_rows = (num_columns + 3) // 4
-
-fig, axes = plt.subplots(num_rows, 4, figsize = (20, num_rows * 5))
-
 # 출력
 ## 결측값 출력
 if missing_values.empty:
@@ -41,31 +33,3 @@ else:
 
 ## 기술 통계량 출력
 print(descriptive_stats.round(3))
-
-## 히스토그램 출력
-for i, column in enumerate(numeric_columns):
-    ax = axes[i // 4, i % 4]
-    data[column].hist(
-        bins = 30,
-        ax = ax,
-        color = 'darkblue'
-        )
-    
-    ax.set_title(f'Histogram of {column}')
-    ax.yaxis.set_major_locator(MaxNLocator(integer = True))
-    ax.yaxis.set_major_locator(plt.MultipleLocator(200))
-
-### 빈 서브플롯 제거
-for j in range(i + 1, num_rows * 4):
-    fig.delaxes(axes[j // 4, j % 4])
-
-### 제목 설정
-fig.suptitle('Histograms of All Numeric Columns', fontsize = 16)
-
-plt.subplots_adjust(
-    hspace = 0.6,
-    wspace = 0.4,
-    top = 0.9,
-    bottom = 0.1
-    )
-plt.show()
